95b2fcd4f7078aa168894442fff127b9056aa3e4
Melisa <rodriguezmelisa93@gmail.com>
2025-04-30 00:09:40 -1200

    agregue clase CajaDeAhorro, creo que puse en algun lado intereses en vez de interes REVISAR

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    //public class CajaDeAhorro : Cuenta{
+    //      private decimal _tasaDeInteres;
+
+    //      public CajaDeAhorro(string numero, decimal saldo, string[] titulares, decimal tasaDeIntereses )
+    //         : base(numero,saldo,titulares)
+    //      {   
+    //            _tasaDeIntereses = tasaDeInteres;
+    //       }
+
+    // G y S
+    //public decimal GetTasaDeInteres() => _tasaDeInteres;
+   // public void SetTasaDeInteres(decimal tasaDeInteres) => _tasaDeInteres = tasaDeInteres;
+
+
+    // public void Depositar(decimal monto){
+    //
+    // }
+    // public void Retirar(decimal monto){
+    //
+    // }
+
+    // public void AplicarIntereses(){
+    //
+    // }
+
+    // 
+    //}
+}
